18274cbe17bab96e25672008ab6ee42d983269ac
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-01-25 04:14:28 +0300

    :sparkles: beta version

--- scripts/formatter.js
+++ scripts/formatter.js
@@ -1,10 +1,18 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+/*
+//chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     console.log(request, sender, sendResponse);
     request.format == "true" ? formatter() : null
     
     sendResponse('test');
-});
+});*/
 
+
+
+
+chrome.runtime.onMessage.addListener((request, sender, response) => {
+    console.log('received', request)
+    request.format == "true" ? formatter() : null
+});
 
 
 function formatter() {
--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,44 @@
 
-const button = document.querySelector('.button');
+const button = document.querySelector('.button')
+/*
+button.addEventListener('click',  function() {
+    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+            chrome.runtime.sendMessage({format: "true"})
+        
+    });
+});*/
+
 
 button.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    return chrome.tabs.query({active: true, currentWindow: true}) 
+        .then(tabs => {
+            return chrome.tabs.sendMessage(tabs[0].id, {format: "true"})
+        })
+        .catch(err => console.log(err))
+})
+
+
+
+
+/*
+button.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, 
+        function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {format: "true"}).then(() => console.log('basarılı')).catch(()=> console.log('error'))
+    })
+
+});
+
+*/
+/*
+button.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, 
+        chrome.tabs.query({active: true, currentWindow: true}, 
+        function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {format: "true"})
     }))
 
 
 });
+
+*/
