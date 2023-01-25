669bff475702991ffe1dc23059252e70679c76f1
Doğan Ay Şengül <20660110052@nevsehir.edu.tr>
2023-01-25 20:19:51 +0300

    :bug: remote branch

--- scripts/formatter.js
+++ scripts/formatter.js
@@ -1,19 +1,7 @@
-/*
-//chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    console.log(request, sender, sendResponse);
-    request.format == "true" ? formatter() : null
-    
-    sendResponse('test');
-});*/
-
-
-
-
 chrome.runtime.onMessage.addListener((request, sender, response) => {
     console.log('received', request)
     request.format == "true" ? formatter() : null
 });
-
 
 function formatter() {
     let textarea = document.querySelector('textarea');
--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,4 @@
-
 const button = document.querySelector('.button')
-/*
-button.addEventListener('click',  function() {
-    chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-            chrome.runtime.sendMessage({format: "true"})
-        
-    });
-});*/
-
 
 button.addEventListener('click', function() {
     return chrome.tabs.query({active: true, currentWindow: true}) 
@@ -18,27 +9,3 @@ button.addEventListener('click', function() {
 })
 
 
-
-
-/*
-button.addEventListener('click', function() {
-        chrome.tabs.query({active: true, currentWindow: true}, 
-        function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {format: "true"}).then(() => console.log('basarılı')).catch(()=> console.log('error'))
-    })
-
-});
-
-*/
-/*
-button.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, 
-        chrome.tabs.query({active: true, currentWindow: true}, 
-        function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {format: "true"})
-    }))
-
-
-});
-
-*/
